b7f18176d027eb787dfa2e74b238fa15a30ca207
Kylian2 <kylian.richard@gmail.com>
2024-09-14 16:24:14 +0200

    TP1: ex2

---
+++ TP1/ex2.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+void affiche(unsigned char tab[], int size){
+    printf("Affichage du tableau : ");
+    for (int i = 0; i < size; i++){
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
+}
+
+int fouiller(unsigned char tab[], int depart, int fin){
+    int i = depart;
+    while( i < fin){
+        if (tab[i] == 0){
+            printf("Child process: %d has found %d at %d\n", getpid(), tab[i], i);
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]){
+
+    //Initialisation du tableau
+    const int TABSIZE = 1000;
+    unsigned char arr[TABSIZE]; //Entier de 0 à 255
+    srandom(time(NULL));
+
+    int N = 1; //correspond au nombre de processus allant fouiller le tableau
+    if (argc > 1) N = atoi(argv[1]); 
+    if (N < 1 || N > 100) N = 1;
+
+    printf("%d will share the task.\n", N);
+    // entasser du foin
+    for (int i = 0; i < TABSIZE; i++){
+        arr[i] = (unsigned char) (random() % 255) + 1;
+    }
+    int index;
+    // cacher l'aiguille
+    printf("Enter a number between 0 and %d: ", TABSIZE-1);
+    scanf(" %d", &index);
+    if (index >= 0 && index < TABSIZE){
+        arr[index] = 0;
+    }else{
+        //Vérification de l'entrée
+        while (!(index >= 0 && index < TABSIZE)){
+            printf("You must a number between 0 and %d: ", TABSIZE-1);
+            scanf(" %d", &index);
+        }
+        arr[index] = 0;
+    }
+
+    //affiche(arr, TABSIZE);
+
+    int rep_fils[N];
+    int status;
+
+    pid_t child = getpid();
+    int i = 0;
+    while (i < N && child > 0){
+        child = fork();
+        if(child == -1){
+            perror("fork() error");
+            exit(1);
+        }
+        if(child > 0){ 
+            //Exécuté par le père
+            wait(&status);
+            printf("i = %d \n", i);
+            rep_fils[i] = WEXITSTATUS(status);
+        }else{
+            //Exécuté par le fils
+            printf("Child process: %d \n", getpid());
+            exit(fouiller(arr, (TABSIZE/N)*i, (TABSIZE/N)*i+TABSIZE/N));
+        }
+        i += 1;
+    }
+    i = 0;
+    bool found = false;
+    while (!found){
+        if (rep_fils[i]){
+            printf("Needle found ! \n");
+            found = true;
+        }
+        i += 1;
+    }
+    if(!found){
+        printf("No needle found ! \n");
+    }
+    
+    
+    //Q1 : srandom(time(NULL)) initalise une seed pour la fonction random() à partir de la date actuelle
+    //Q2 : Il ne peut pas y avoir de 0 à la fin de l'entassement du foin car on ajoute toujours 1 en plus au nombre généré
+    //Q3 : Pour passer une information du fils au père, on peut se servir du status renvoyé par le fils lors de sa mort,
+    //     le père pourra le récupérer avec WEXITSTATUS
+
+    //Le programme peut etre tester avec : 
+    //0 -> premier indice (cherché par le père)
+    //999 -> dernier indice (cherché par le fils)
+    //500 -> TABSIZE/2 (moment de la séparation entre le père et le fils)
+
+    return 0;
+}
--- compile.sh
+++ compile.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Usage : ./script.sh chemin dossier serror|werror [options_execution...]
+
 if [ "$3" = "serror" ]; then
-    clear && gcc -Wall ./"$1"/"$2".c -o ./"$1"/output/ex1 && ./"$1"/output/ex1
+    # Compilation avec seulement -Wall
+    clear && gcc -Wall ./"$1"/"$2".c -o ./"$1"/output/"$2" && ./"$1"/output/"$2" "${@:4}"
+elif [ "$3" = "werror" ]; then
+    # Compilation avec -Wall et -Werror
+    clear && gcc -Wall -Werror ./"$1"/"$2".c -o ./"$1"/output/"$2" && ./"$1"/output/"$2" "${@:4}"
 else
-    clear && gcc -Wall -Werror ./"$1"/"$2".c -o ./"$1"/output/ex1 && ./"$1"/output/ex1
+    echo "Troisième argument non valide : utilisez 'serror' ou 'werror'."
 fi
