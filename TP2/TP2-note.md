f6549f9eb72de6cd4fb19059ce6d478d3ec1eb7b
Kylian2 <kylian.richard@gmail.com>
2024-09-30 10:28:22 +0200

    TP2: chat

--- TP2/TP2-note.md
+++ TP2/TP2-note.md
@@ -12,3 +12,17 @@ Dans ce schéma, le serveur est `tirage` et le client est `pi`. Car `pi` interro
 **Pourquoi les sockets stream (par exemple, via TCP) sont mieux adaptées à ce type d’application ?** 
 
 En mode socket stream, je suis connecté,il y une fiabilité de la connexion, si le message n'est pas reçu, le message sera redemandé. Cela permet également de savoir combien de clients sont connectés pour leur envoyer les messages. On peut également gérer les déconnexions.
+
+**Testez l’application. Quel port utilise cette application pour communiquer (utilisez la commande ss et sa page de man pour le découvrir) ? Quelle option de ss avez-vous utilisé ?**
+
+Avec la commende `ss -pant` pour afficher tout les ports en écoute. 
+Le serveur écoute toutes les adresses sur le port 5015.
+
+| State | Recv-Q | Send-Q | Local Address:Port | Peer Address:Port | Port
+| :--- | :--- | :--- | :--- | :--- | :--- |
+| LISTEN | 0 | 128 | 0.0.0.0:5015 | 0.0.0.0:* | users:(("chat_serveur",pid=6110,fd=5),("chat_serveur",pid=5531,fd=5)) |  
+ESTAB  | 0  |0  |  127.0.0.1:5015 | 127.0.0.1:54050 |  users:(("chat_serveur",pid=6110,fd=6),("chat_serveur",pid=5531,fd=6))|
+|  ESTAB   | 0 |0|127.0.0.1:54050 |127.0.0.1:5015 | users:(("chat_client",pid=6109,fd=3),("chat_client",pid=6108,fd=3))                      
+
+
+**De combien de processus l’application serveur est-elle composée pour gérer 1, 2, n clients (utilisez la commande ps et sa page de man pour le découvrir) ?**
--- TP2/chat/client.c
+++ TP2/chat/client.c
@@ -1,6 +1,8 @@
 /* CLIENT. Donner l'adresse IP et un pseudo en paramètre */
 /* exemple "client 127.0.0.1 dr.ced", lancer en dernier. */
 
+/* snprintf(message_a_envoyer, BUFFERSIZE, "%s >", argv[2])*/
+
 #include <stdio.h>             /* fichiers d'en-tête classiques */
 #include <stdlib.h>
 #include <string.h>
@@ -18,13 +20,70 @@
 
 int main(int argc, char *argv[]) {
 
+  char message[BUFFER_SIZE] = {0};
+
   /* 1. On crée la socket du client. */
+
+  int sclient = socket(AF_INET, SOCK_STREAM, 0);
+  if(sclient ==-1){
+    perror("Erreur de socket sclient");
+    return -1;
+  }
+
+  struct sockaddr_in saddr = {0};
 
   /* 2. On prépare l'adresse du serveur. */
 
+  saddr.sin_family = AF_INET;
+  saddr.sin_addr.s_addr = inet_addr(argv[1]);
+  saddr.sin_port = htons(PORT_SERVEUR);
+
   /* 3. On demande une connexion au serveur, tant qu'on y arrive pas. */
 
+  printf("Connexion...");
+  while(connect(sclient, (struct sockaddr *) &saddr, sizeof(saddr)) == -1){printf(".");};
+
+  if(fork() == 0){
+    while(1){
+      int nbLus = read(sclient, message, BUFFER_SIZE-1);
+      if (nbLus == -1){
+        perror("Erreur lors du read fils");
+      }
+      if (nbLus == 0){
+        break;
+      }
+
+      //Ecriture sur la sortie
+      write(1,message, BUFFER_SIZE);
+
+    }
+    shutdown(sclient, SHUT_RDWR);
+    close(sclient);
+    exit(0);
+  }
+
+  while(1){
+    int nbLus = read(0, message, BUFFER_SIZE-1); 
+    if (nbLus == 0){ //Connexion fermé
+        break;
+    }
+    if (nbLus == -1){
+        perror("Erreur lors du readB");
+        return 1;
+    }
+    //printf("%s > ", argv[2]);
+    snprintf(message, BUFFER_SIZE-1, "%s >", argv[2]);
+    write(sclient, &message, BUFFER_SIZE-1);
+  }
+
+  shutdown(sclient, SHUT_RDWR);
+  close(sclient);
+
   /* 4. On communique. */
+
+  /* Le fils lit les données reçus depuis le serveur et les affiches sur la sortie standard*/
+
+  /* Le père lit les données de l'entrée standard et les envoie au serveur*/
 
   /* 5. On termine et libère les ressources. */
 
