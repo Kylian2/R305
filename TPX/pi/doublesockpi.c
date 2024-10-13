8c8a276a513a10824a37776b75ce422d4d4b6ebc
Kylian2 <kylian.richard@gmail.com>
2024-10-13 21:52:48 +0200

    pi: exercice refait pour entrainement

--- TPX/pi/doublesockpi.c
+++ TPX/pi/doublesockpi.c
@@ -11,14 +11,12 @@
 int main(){
 
     int nbLus = 0;
-    char message[BUFFER_SIZE];
-    int x;
-    int y;
-    int pi;
+    double x;
+    double y;
+    double pi;
     int n = 0;
     int m = 0; //le nombre tirage tq x^2+y^2 <= 1
     char c = '\0';
-
     /* 1. Création de la socketpair */
 
     int sockfd[2]; //Le tableau contenant des descripteurs de la socket pair
@@ -27,17 +25,14 @@ int main(){
         perror("Erreur lors de la creation de la socket");
         return 1;
     }
-
     /* Le fils */
     if(fork() == 0){
-        shutdown(sockfd[0], SHUT_RDWR);
+        //shutdown(sockfd[0], SHUT_RDWR);
         close(sockfd[0]);
-
         while (1){
             n++;
             write(sockfd[1], &c, sizeof(char));
-            nbLus = read(sockfd[1], &x, sizeof(x));
-
+            nbLus = read(sockfd[1], &x, sizeof(double));
             if(nbLus == -1){
                 perror("Erreur de read X");
                 return 1;
@@ -47,7 +42,7 @@ int main(){
             }
 
             write(sockfd[1], &c, sizeof(char));
-            nbLus = read(sockfd[1], &y, sizeof(y));
+            nbLus = read(sockfd[1], &y, sizeof(double));
 
             if(nbLus == -1){
                 perror("Erreur de read Y");
@@ -62,32 +57,29 @@ int main(){
             }
 
             if(n%10000 == 0){
-                pi = 4*m/n;
-                printf("Valeur de pi %d", pi);
+                pi = 4.0*m/n;
+                printf("Valeur de pi %g \n", pi);
             }
 
-            if(n == MAX_TIRAGE){
-                pi = 4*m/n;
-                printf("Valeur finale de pi %d", pi);
+            if(n >= MAX_TIRAGE){
+                pi = 4.0*m/n;
+                printf("Valeur finale de pi %g \n", pi);
                 break;
             }
-
         }
         shutdown(sockfd[1], SHUT_RDWR);
         close(sockfd[1]);
         exit(0);
     }
-
     /* Le père - effectue les tirages*/
-    shutdown(sockfd[1], SHUT_RDWR);
+    //shutdown(sockfd[1], SHUT_RDWR);
     close(sockfd[1]);
 
     srand(time(NULL));
     double z = (double) random()/RAND_MAX;
 
     while(1){
-        read(sockfd[0], message, sizeof(char));
-
+        nbLus = read(sockfd[0], &c, sizeof(char));
         if(nbLus == -1){
             perror("Erreur de read demande");
             return 1;
@@ -95,9 +87,8 @@ int main(){
         if(nbLus == 0){
             break;
         }
-
         z = (double) random()/RAND_MAX;
-        write(sockfd[0], &z, sizeof(z));
+        write(sockfd[0], &z, sizeof(double));
     }
 
     shutdown(sockfd[0], SHUT_RDWR);
